2e2784d8a90428c0bb4f54592a693616b120e576
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-01-18 14:28:32 +0500

    added welcome and footer comp

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -24,59 +24,65 @@ const JobTypes = [
   "Freelance",
   "Other",
 ]
-const Welcome = () => {
-  const router = useRouter();
-  const [activeJobType, setActiveJobType] = useState("Full Time");
+const Welcome = ({ searchTerm, setSearchTerm,handlePress }) => {
+	const router = useRouter();
+	const [activeJobType, setActiveJobType] = useState("Full Time");
 	return (
-	
-			<View style={styles.container}>
-				<Text style={styles.userName}>Welcome Taimoor</Text>
-				<Text style={styles.welcomeMessage}>
-					Find your perfect job match
-				</Text>
-				<View style={styles.searchContainer}>
-					<View style={styles.searchWrapper}>
-						<TextInput
-							placeholder="what are you looking for?"
-              style={styles.searchInput}
-              value=""
-              onChange={() => { }}
-              
-						/>
-        </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => router.push("/search")}>
-          <Image
-            source={icons.search}
-            style={styles.searchBtnImage}
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.tabsContainer}>
-        <FlatList
-          data={JobTypes}
-          renderItem={({ item }) => (
-            <TouchableOpacity style={styles.tab(activeJobType, item)}
-              onPress={() => {
-                setActiveJobType(item)
-                router.push(`/search/${item}`)
-              }}
-            >
-              <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
-            </TouchableOpacity>
-          )} 
-          keyExtractor={(item) => item}
-          contentContainerStyle={{
-            columnGap:SIZES.small,
-          }}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        />
-
-        
-      </View>
+		<View style={styles.container}>
+			<Text style={styles.userName}>Welcome Taimoor</Text>
+			<Text style={styles.welcomeMessage}>
+				Find your perfect job match
+			</Text>
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput
+						placeholder="what are you looking for?"
+						style={styles.searchInput}
+						value={searchTerm}
+						onChangeText={(text) => setSearchTerm(text)}
+					/>
+				</View>
+				<TouchableOpacity
+					style={styles.searchBtn}
+					onPress={handlePress}
+				>
+					<Image
+						source={icons.search}
+						style={styles.searchBtnImage}
+						resizeMode="contain"
+					/>
+				</TouchableOpacity>
 			</View>
-		
+			<View style={styles.tabsContainer}>
+				<FlatList
+					data={JobTypes}
+					renderItem={({ item }) => (
+						<TouchableOpacity
+							style={styles.tab(activeJobType, item)}
+							onPress={() => {
+								setActiveJobType(item);
+								router.push(`/search/${item}`);
+							}}
+						>
+							<Text
+								style={styles.tabText(
+									activeJobType,
+									item
+								)}
+							>
+								{item}
+							</Text>
+						</TouchableOpacity>
+					)}
+					keyExtractor={(item) => item}
+					contentContainerStyle={{
+						columnGap: SIZES.small,
+					}}
+					horizontal
+					showsHorizontalScrollIndicator={false}
+				/>
+			</View>
+		</View>
 	);
 };
 
--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 
 import styles from './tabs.style'
+import { SIZES } from "../../../constants";
 
-const Tabs = () => {
-  return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
-  )
-}
+
+const TabButton = ({ name, activeTab, onHandleSearch }) => {
+	return (
+		<TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearch}
+    >
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+		</TouchableOpacity>
+	);
+};
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={tabs}
+				renderItem={({ item }) => (
+					<TabButton
+						name={item}
+						activeTab={activeTab}
+						onHandleSearch={() => setActiveTab(item)}
+					/>
+				)}
+				keyExtractor={(item) => item}
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				contentContainerStyle={{
+					columnGap: SIZES.small / 2,
+				}}
+			/>
+		</View>
+	);
+};
 
 export default Tabs
